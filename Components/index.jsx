71860652c5ec130f19a9ee62a0f67901b77f9036
Siege211 <Siege211@morris.umn.edu>
2019-01-02 21:52:43 -0600

    3 out of 4 components Rendering

--- Components/index.jsx
+++ Components/index.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import MusicPlayer from './Jane_MusicPlayer_Component/client/src/components/MusicPlayer.jsx';
+ import CommentSection from './siege211Component-Service/client/src/index.jsx';
+ import SongInfo from './steven-songsInfo-module/client/src/components/App.jsx';
+ import Sidebar from './JustinComponent-Service/client/index.jsx';
+
 
 class DemocritusCloud extends React.Component{
   constructor(props){
@@ -7,12 +12,12 @@ class DemocritusCloud extends React.Component{
   }
 
   render(){
-      return (<div>
-          <JaneComp/>
-          <ThomComp/>
-          <StevenComp/>
-          <JustinComp/>
-      </div>)
+      return (
+        <div>
+         <CommentSection />
+         <SongInfo />
+         <Sidebar />
+        </div>)
   }
 
 }
